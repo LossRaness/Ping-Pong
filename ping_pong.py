140d0115b97a9f021f10fac187b9799427db5bdb
Azalia <lossranes@gmail.com>
2025-05-10 18:07:56 +0400

    creat pyfile

---
+++ ping_pong.py
@@ -0,0 +1,31 @@
+#Создай собственный Шутер!
+from pygame import *
+
+
+#класс родитель для спрайтов
+class GameSprite(sprite.Sprite):
+    #коструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+
+        #каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        #каждый спрайт далжен хранить свойство rect - прямоугольник, в который внисан спрайт
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+#класс игрока    
+class Player(GameSprite):
+    #метод управление с клавиатуры
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+    
