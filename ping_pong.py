5d7ae6626648c1bfacb7ff4f35b3230e3b50fb17
Azalia <lossranes@gmail.com>
2025-05-10 18:58:08 +0400

    заготовка игры Ping-pong

--- ping_pong.py
+++ ping_pong.py
@@ -22,10 +22,49 @@ class GameSprite(sprite.Sprite):
 #класс игрока    
 class Player(GameSprite):
     #метод управление с клавиатуры
-    def update(self):
+    def update_r(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-    
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 150:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_width - 150:
+            self.rect.y += self.speed
+
+#Игровая сцена
+back = (200, 255, 255) #фон
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+#флаги, отвечающие за состояние изры
+run = True
+finish = False
+clock = time.Clock()
+FPS = 60
+#создание ракетки
+racket1 = Player('racket.png', 10, 200, 50, 150, 4)
+racket2 = Player('racket.png', 540, 200, 50, 150, 4)
+
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
+
+    display.update()
+    clock.tick(FPS)
+  
